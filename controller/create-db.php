aed70ab8770e2e8b2d0d576ba0b7667bc592bb2d
Jourdan Wallace <jaywallace713@gmail.com>
2015-01-28 09:23:49 -0800

    finished milestone 1

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,8 +1,11 @@
 <?php
+    require_once("../model/database.php");
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    $connection = new mysqli($host, $username, $password);
+    
+    if($connection->connect_error) {
+        die("Error: " . $connection->connect_error);
+    }
+    else{
+        echo "Success";
+    }
