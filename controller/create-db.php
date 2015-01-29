0aa6349e718bfcdc31860fc0dfde397e714b137a
Jourdan Wallace <jaywallace713@gmail.com>
2015-01-29 09:15:56 -0800

    Got my database up and running. YAY! Achievement.

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,11 +1,21 @@
 <?php
-    require_once("../model/database.php");
+    require_once(__DIR__ . "/../model/database.php");
 
     $connection = new mysqli($host, $username, $password);
     
     if($connection->connect_error) {
         die("Error: " . $connection->connect_error);
     }
-    else{
-        echo "Success";
+    
+    $exists = $connection->select_db($database);
+    
+    if(!$exists) {
+        $query = $connection->query("CREATE DATABASE $database");
+        if($query) {
+            echo "Successfully created database: " . $database;
+        }
     }
+    
+    
+    
+    $connection->close();
