5b04a1cf98c4aa068e2fd9d4366e6ad71c550dae
Jourdan Wallace <jaywallace713@gmail.com>
2015-02-20 09:20:05 -0800

    Something isn't quite right, my login isn't coming up.

--- controller/read-posts.php
+++ controller/read-posts.php
@@ -7,10 +7,10 @@
     
     if($result) {
         while($row = mysqli_fetch_array($result)) {
-            echo "<div class ='posts'>";
-            echo "<h2> . $row['title'] . </h2>";
+            echo "<div class ='post'>";
+            echo "<h2>" . $row['title'] . "</h2>";
             echo "<br />";
-            echo "<p> . $row['title'] . </h1>";
+            echo "<p>" . $row['post'] . "</h1>";
             echo "<br/>";
             echo "</div";
         }
